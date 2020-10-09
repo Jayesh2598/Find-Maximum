c2d006567145b08868f85f1723d52aec83dd09e4
Jay002 <jayesh2598@gmail.com>
2020-10-09 23:18:43 +0530

    Modified: Maximum from more than 3 values can be obtained.

--- main/java/com/capgemini/findMax/FindMaximum.java
+++ main/java/com/capgemini/findMax/FindMaximum.java
@@ -1,56 +1,26 @@
 package com.capgemini.findMax;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class FindMaximum<E extends Comparable<E>> {
 
 	static Scanner SC = new Scanner(System.in);
 
-	E a, b, c;
+	E maximum;
 
-	public FindMaximum(E a, E b, E c) {
-		this.a = a;
-		this.b = b;
-		this.c = c;
-	}
-
-	public E testMaximum() {
-		return findMaximum(a, b, c);
+	public E getMaximum() {
+		return maximum;
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Welcome!");
+		System.out.println("Welcome to Test Maximum program!");
 
-		System.out.println("Enter three Integers:");
-		int a1 = SC.nextInt();
-		int a2 = SC.nextInt();
-		int a3 = SC.nextInt();
-		FindMaximum<Integer> maxInt = new FindMaximum<Integer>(a1, a2, a3);
-		System.out.println("The maximum of the three Integers is: " + maxInt.testMaximum());
-
-		System.out.println("Enter three Floats:");
-		Float b1 = SC.nextFloat();
-		Float b2 = SC.nextFloat();
-		Float b3 = SC.nextFloat();
-		FindMaximum<Float> maxFloat = new FindMaximum<Float>(b1, b2, b3);
-		System.out.println("The maximum of the three Floats is: " + maxFloat.testMaximum());
-
-		System.out.println("Enter three Strings:");
-		String c1 = SC.next();
-		String c2 = SC.next();
-		String c3 = SC.next();
-		FindMaximum<String> maxString = new FindMaximum<String>(c1, c2, c3);
-		System.out.println("The maximum of the three Strings is: " + maxString.testMaximum());
 	}
 
-	public static <E extends Comparable<E>> E findMaximum(E a, E b, E c) {
-		E maximum;
-		if (a.compareTo(b) > 0 && a.compareTo(c) > 0)
-			maximum = a;
-		else if (b.compareTo(a) > 0 && b.compareTo(c) > 0)
-			maximum = b;
-		else
-			maximum = c;
+	public E findMaximum(E... values) {
+		Arrays.sort(values);
+		this.maximum = values[values.length - 1];
 		return maximum;
 	}
 }
--- test/java/com/capgemini/testFindMax/testFindMaximum.java
+++ test/java/com/capgemini/testFindMax/testFindMaximum.java
@@ -6,33 +6,37 @@ import com.capgemini.findMax.FindMaximum;
 
 public class testFindMaximum {
 
+	FindMaximum<Integer> maxInt = new FindMaximum<>();
+	FindMaximum<Float> maxFloat = new FindMaximum<>();
+	FindMaximum<String> maxString = new FindMaximum<>();
+
 	@Test
 	public void maximumAtFirstPosition() {
-		Integer maxInteger = FindMaximum.findMaximum(50, 30, -10);
-		assertTrue(50 == maxInteger);
-		Float maxFloat = FindMaximum.findMaximum(50.0f, 30.0f, -10.0f);
-		assertTrue(50.0f == maxFloat);
-		String maxString = FindMaximum.findMaximum("Peach", "Apple", "Banana");
-		assertTrue("Peach".equals(maxString));
+		Integer maxinteger = maxInt.findMaximum(50, 30, -10);
+		assertTrue(50 == maxinteger);
+		Float maxfloat = maxFloat.findMaximum(50.0f, 30.0f, -10.0f);
+		assertTrue(50.0f == maxfloat);
+		String maxstring = maxString.findMaximum("Peach", "Apple", "Banana");
+		assertTrue("Peach".equals(maxstring));
 	}
 
 	@Test
 	public void maximumAtSecondPosition() {
-		Integer maxInteger = FindMaximum.findMaximum(30, 50, -10);
-		assertTrue(50 == maxInteger);
-		Float maxFloat = FindMaximum.findMaximum(30.0f, 50.0f, -10.0f);
-		assertTrue(50.0f == maxFloat);
-		String maximum = FindMaximum.findMaximum("Apple", "Peach", "Banana");
-		assertTrue("Peach".equals(maximum));
+		Integer maxinteger = maxInt.findMaximum(30, 50, -10);
+		assertTrue(50 == maxinteger);
+		Float maxfloat = maxFloat.findMaximum(30.0f, 50.0f, -10.0f);
+		assertTrue(50.0f == maxfloat);
+		String maxstring = maxString.findMaximum("Apple", "Peach", "Banana");
+		assertTrue("Peach".equals(maxstring));
 	}
 
 	@Test
 	public void maxAtFirstPosition() {
-		Integer maxInteger = FindMaximum.findMaximum(30, -10, 50);
-		assertTrue(50 == maxInteger);
-		Float maxFloat = FindMaximum.findMaximum(30.0f, -10.0f, 50.0f);
-		assertTrue(50.0f == maxFloat);
-		String maximum = FindMaximum.findMaximum("Apple", "Banana", "Peach");
-		assertTrue("Peach".equals(maximum));
+		Integer maxinteger = maxInt.findMaximum(30, -10, 50);
+		assertTrue(50 == maxinteger);
+		Float maxfloat = maxFloat.findMaximum(30.0f, -10.0f, 50.0f);
+		assertTrue(50.0f == maxfloat);
+		String maxstring = maxString.findMaximum("Apple", "Banana", "Peach");
+		assertTrue("Peach".equals(maxstring));
 	}
 }
